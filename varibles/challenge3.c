073ea394a9b593f632f381ceab8cbb91a3b3dc6e
AjartiJaouad <ajartijaouad14@gmail.com>
2024-09-09 17:55:50 +0200

    challenge2

---
+++ varibles/challenge3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+float Vkm ,Vms;
+printf("Donnez la vitesse en kilomètres par heure (km/h) :");
+scanf("%f",&Vkm);
+    Vms = Vkm * 0.27778;
+printf("le tronasfe de la vitesse en metres paer socond et VITESSSE_M/S =%.2f",Vms);
+    return 0;
+    
+}
