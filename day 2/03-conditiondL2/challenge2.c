342be72acddc7dc9ffaf1fbf4bd561a64c79ab20
AjartiJaouad <ajartijaouad14@gmail.com>
2024-09-10 17:33:09 +0200

    challenge2.c

--- day 2/03-conditiondL2/challenge2.c
+++ day 2/03-conditiondL2/challenge2.c
@@ -1,47 +1,48 @@
 #include <stdio.h>
-int main (){
-    int Age , NumbreDeac  ,typVoituer;
-    float PrimddeBas ,Prime;
-     
-    printf(" Calcule de Prime d'Assurance Aouto\n");
-    printf(" voutre Primedebas");
-    scanf("%f",&PrimddeBas);
-    printf("entrer voutre Age du conducteur  :");
-    scanf("%d",&Age);
-    printf("entrer voutre typ devoitueur  :");
-    scanf("%d",&typVoituer);
-    printf("entrer le nombre d'adccidents  au cours de 5 déernier anneées  :");
-    scanf("%d",&NumbreDeac);
-    if (Age<25){
-      
-      Prime=PrimddeBas*1.5;
 
-    }else if (Age<=65) {
-      Prime=PrimddeBas;
+int main() {
+    int Age, NumbreDeac, typVoituer;
+    float PrimddeBas, Prime;
 
-    }else{
-              Prime=PrimddeBas*1.2;
+    printf("Calcul de Prime d'Assurance Auto\n");
+    printf("Entrez votre Prime de base: ");
+    scanf("%f", &PrimddeBas);
+    printf("Entrez votre âge du conducteur: ");
+    scanf("%d", &Age);
+    printf("Entrez le type de voiture (1, 2 ou 3): ");
+    scanf("%d", &typVoituer);
+    printf("Entrez le nombre d'accidents au cours des 5 dernières années: ");
+    scanf("%d", &NumbreDeac);
 
-    };
-    switch (typVoituer)
-    {
-    case '1'  :Prime=Prime*2;
-        
-        break;
-    case '2'  :Prime=Prime*1.2;
-        
-        break;
-    case '3'  :Prime=Prime*1.1;
-        
-        break;
-    
-    default:
-        break;
-    };
-    if (NumbreDeac>1){
-        Prime=Prime+PrimddeBas*1.3;
+    if (Age < 25) {
+        Prime = PrimddeBas * 1.5;
+    } else if (Age <= 65) {
+        Prime = PrimddeBas;
+    } else {
+        Prime = PrimddeBas * 1.2;
     }
-    printf("Prime:%f",Prime);
- 
+
+    switch (typVoituer) {
+        case 1: 
+            Prime = Prime * 2;
+            break;
+        case 2: 
+            Prime = Prime * 1.2;
+            break;
+        case 3: 
+            Prime = Prime * 1.1;
+            break;
+        default:
+            printf("Type de voiture invalide.\n");
+            return 1; 
+    }
+
+    
+    if (NumbreDeac > 1) {
+        Prime = Prime + PrimddeBas * 1.3;
+    }
+
+    printf("Prime: %.2f\n", Prime);
+
     return 0;
 }
