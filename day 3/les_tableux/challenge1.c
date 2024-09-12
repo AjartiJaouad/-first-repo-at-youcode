8c88cb4578fe937e7e96952af7c8bf82afe58488
AjartiJaouad <ajartijaouad14@gmail.com>
2024-09-12 12:48:35 +0200

    challenge1

---
+++ day 3/les_tableux/challenge1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() {
+
+int i, N=6;
+int T[6]={1,2,3,4,5};
+    printf("---Affichage dun tabeleaux---");
+    for (i=0;i<5;i++){
+        printf("les Tableux T[%d] =%d\n",i,T[i]);
+
+    }
+ 
+    
+    return 0;
+}
