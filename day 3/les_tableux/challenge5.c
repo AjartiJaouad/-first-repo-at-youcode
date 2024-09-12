c3870991386fe89b9cb5d028133904fb68814881
AjartiJaouad <ajartijaouad14@gmail.com>
2024-09-12 15:04:48 +0200

    challenge5

---
+++ day 3/les_tableux/challenge5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+int main() {
+
+int nombre_delemon ,min ,i;
+    printf("\n---Affichage les element  d'un tabeleaux---\n");
+    printf("lentrer le nombre délemment\n");
+   scanf("%d",&nombre_delemon);
+   int T[nombre_delemon];
+
+   for (i=0;i<nombre_delemon;i++){
+    printf("saisir les elemen de tableaux\n");
+       scanf("%d",&T[i]);
+
+   };
+   min=T[0];
+   for (i=0;i<nombre_delemon;i++){
+   
+   if (min >T[i]){
+    min=T[i];
+   }
+    
+   };
+  
+
+   for (i=0;i<nombre_delemon;i++){
+    
+   printf(" les éléments du tableau  son :T[%d]=%d\n",i,T[i]);
+printf(" la min de elelment de tableux est :%d\n",min);
+   };
+     
+
+   
+
+    
+    return 0;
+}
