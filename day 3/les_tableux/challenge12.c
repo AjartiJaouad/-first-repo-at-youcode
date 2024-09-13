0b8bb8fe0f33c11daa633c220cd13e63d3376936
AjartiJaouad <ajartijaouad14@gmail.com>
2024-09-13 11:26:01 +0200

    challenge12

---
+++ day 3/les_tableux/challenge12.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int main() {
+
+int nombre_delemon, i;
+    printf("\n---Afficher les element Paire---\n");
+    printf("lentrer le nombre délemment\n");
+   scanf("%d",&nombre_delemon);
+   int T[nombre_delemon],T_inversee[nombre_delemon];
+
+   for (i=0;i<nombre_delemon;i++){
+    printf("saisir les elemen de tableaux\n");
+       scanf("%d",&T[i]);
+
+   };
+
+  printf("les elements paire sont:");
+int c = 0; 
+
+    for (i = 0; i < nombre_delemon; i++) {
+        if (T[i] % 2 == 0) {
+            printf("%d ", T[i]);
+            c = 1;
+        };
+    };
+
+    if (!c) {
+        printf("Aucun element.");
+    };
+
+}
